55c10351f2191f7b0af264333021dbe4a7e00a70
FRANKLIN YIMY PEZO MONTESINOS <42632661@continental.edu.pe>
2025-04-03 11:08:00 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
-import siteConfig from './src/data/site-config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site:'https://Franklinpm.github.io',
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [mdx(), sitemap()]
+    site: 'https://franklinpm.github.io',
+    integrations: [
+        mdx(),
+        sitemap(),
+        tailwind({
+            applyBaseStyles: false
+        })
+    ]
 });
